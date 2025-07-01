067d0accf2415571d72ca2a203d56eeefde9f83c
SR <savinien.rossi@carbon-it.com>
2025-07-01 17:37:28 +0200

    tests: add basic tests

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -11,6 +11,20 @@ public class StringCalculatorTest {
     }
 
     @Test
+    public void testAddOneNumberString() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(1, calc.add("1"));
+    }
+
+    @Test
+    public void testAddTwoNumbersString() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(5, calc.add("2,3"));
+    }
+
+
+
+    @Test
     public void testAddNoneNumberString() {
         StringCalculator calc = new StringCalculator();
        assertThrows(IllegalArgumentException.class, () -> calc.add("a,b"));
