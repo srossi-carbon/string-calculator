1c2318209c3b00a1e6002b2bc3b3324b5f1674dd
SR <savinien.rossi@carbon-it.com>
2025-07-01 17:35:41 +0200

    tests: add non numbers test to throw exception

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class StringCalculatorTest {
@@ -6,6 +8,12 @@ public class StringCalculatorTest {
     public void testAddEmptyString() {
         StringCalculator calc = new StringCalculator();
         assertEquals(0, calc.add(""));
+    }
+
+    @Test
+    public void testAddNoneNumberString() {
+        StringCalculator calc = new StringCalculator();
+       assertThrows(IllegalArgumentException.class, () -> calc.add("a,b"));
     }
 
 
