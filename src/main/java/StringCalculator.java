f1d86f651c000106fd7c2454c2a042f325c5777d
SR <savinien.rossi@carbon-it.com>
2025-07-02 16:19:50 +0200

    refactor: add constants to class and extract sum Function

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,12 +4,14 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 
+    private static final int MAX_INT_VALUE = 1000;
+    private static final String DEFAULT_DELIMITERS = ",\n";
+
+
     public static int add(String input) {
-        int sum = 0;
+        if (input.isEmpty()) {return 0;}
 
-        if (input.isEmpty()) {return sum;}
-
-        String delimitersRegex = "[,\n]";
+        String delimiters = DEFAULT_DELIMITERS;
         String calculatorStr = input;
 
         Pattern pattern = Pattern.compile("^//(.)\\n(.*)", Pattern.DOTALL);
@@ -18,33 +20,46 @@ public class StringCalculator {
         if (matcher.matches()) {
             String specialDelimiter = matcher.group(1);
             calculatorStr = matcher.group(2);
-            delimitersRegex = "[,\n" + specialDelimiter + "]";
+            delimiters = DEFAULT_DELIMITERS + specialDelimiter;
         }
 
-        int[] elementsToSum = Arrays.stream(calculatorStr.split(delimitersRegex))
+        int[] elementsToSum = Arrays.stream(calculatorStr.split("[" + delimiters + "]"))
                 .map(String::trim)
                 .mapToInt(Integer::parseInt)
                 .toArray();
 
         int [] negativeNumbers = Arrays.stream(elementsToSum).filter(element -> element < 0).toArray();
+
         if (negativeNumbers.length > 0) {
-            throw new IllegalArgumentException("Les nombres négatifs ne sont pas autorisés : " +
-                    Arrays.stream(negativeNumbers)
-                            .mapToObj(String::valueOf)
-                            .collect(Collectors.joining(", ")));
-        }
-        for (Integer element : elementsToSum) {
-            if (element <= 1000) {
-                sum += element;
-            }
+             String negativeNumberErrorMessage = "Les nombres négatifs ne sont pas autorisés";
+             if (negativeNumbers.length > 1) {
+                 negativeNumberErrorMessage += " : " + Arrays.stream(negativeNumbers)
+                         .mapToObj(String::valueOf)
+                         .collect(Collectors.joining(", "));
+             }
+            throw new IllegalArgumentException(negativeNumberErrorMessage);
+
         }
 
-        return sum;
+
+        return sumNumberUnderMaxValue(elementsToSum);
     }
 
 
+    private static Integer sumNumberUnderMaxValue(int [] values) {
+        int sum = 0;
+
+        for (Integer element : values) {
+            if (element <= StringCalculator.MAX_INT_VALUE) {
+                sum += element;
+            }
+        }
+        return sum;
+    };
+
+
     public static void main(String[] args) {
-        String input = "";
+        String input = "2,3";
 
         int sum = add(input);
         System.out.println("Somme = " + sum);
